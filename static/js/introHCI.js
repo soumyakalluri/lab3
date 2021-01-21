4b09855d3f000d535aa9977573f3d9ce1b915784
Soumya Kalluri <soumya.kalluri11@gmail.com>
2021-01-21 00:19:14 -0800

    pushing so i can connect to heroku

--- static/js/introHCI.js
+++ static/js/introHCI.js
@@ -10,9 +10,26 @@ $(document).ready(function() {
  */
 function initializePage() {
 	$("#testjs").click(function(e) {
-		$('.jumbotron h1').text("Javascript is connected");
+		$('.jumbotron h1').text("bruh why'd you click the button. you've ruined it.");
+		$("#testjs").text("Please wait...");
+		//$(".jumbotron p").addClass("active");
+		$(".jumbotron p").toggleClass("active");
 	});
+
+	$("a.thumbnail").click(projectClick);
 
 	// Add any additional listeners here
 	// example: $("#div-id").click(functionToCall);
+}
+
+function projectClick(e) {
+    e.preventDefault();
+	$(this).css("background-color", "#7fff00");
+	var containingProject = $(this).closest(".project");
+    var description = $(containingProject).find(".project-description");
+    if (description.length == 0) {
+       $(containingProject).append("<div class='project-description'><p>Description of the project.</p></div>");
+    } else {
+		$(containingProject).find(".project-description").fadeOut();
+	}
 }
